781cb676965a9e5c653aeb56c8d029720709cd11
mohan1899 <mohanmk1899@gmail.com>
2022-10-09 12:13:41 +0530

    Added Prod URL

--- post-code-ui/src/BaseService/base-service.ts
+++ post-code-ui/src/BaseService/base-service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://localhost:44398/', //'https://v83jwokeh2.execute-api.us-east-1.amazonaws.com/Prod/api',
+  baseURL: 'https://j5utrffkv3.execute-api.us-east-1.amazonaws.com/Prod/api',
   headers: {
     "Content-type": "application/json"
   }
