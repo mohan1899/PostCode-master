85397052e5a310bb17173ec960c22b0e7e8afc12
mohan1899 <mohanmk1899@gmail.com>
2022-10-09 12:23:48 +0530

    Removed Value Controller

--- post-code-ui/src/BaseService/base-service.ts
+++ post-code-ui/src/BaseService/base-service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'https://j5utrffkv3.execute-api.us-east-1.amazonaws.com/Prod/api',
+  baseURL: 'https://j5utrffkv3.execute-api.us-east-1.amazonaws.com/Prod',
   headers: {
     "Content-type": "application/json"
   }
